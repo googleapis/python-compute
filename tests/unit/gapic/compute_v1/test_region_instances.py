229d59f845c662df0df8c1e3f081f8c42d2de66e
yoshi-automation <yoshi-automation@google.com>
2021-08-05 23:08:09 +0000

    chore!: Migrate to gapic-generator-java, remove monolith dependency
    Source-Author: Vadym Matsishevskyi <25311427+vam-google@users.noreply.github.com>
    Source-Date: Wed Jun 30 10:37:13 2021 -0700
    Source-Repo: googleapis/googleapis-discovery
    Source-Sha: abf4cec1ce9e02e4d7d650bf66137c347cdd0d44
    Source-Link: https://github.com/googleapis/googleapis-discovery/commit/abf4cec1ce9e02e4d7d650bf66137c347cdd0d44

--- tests/unit/gapic/compute_v1/test_region_instances.py
+++ tests/unit/gapic/compute_v1/test_region_instances.py
@@ -123,7 +123,22 @@ def test_region_instances_client_service_account_always_use_jwt(client_class):
     ) as use_jwt:
         creds = service_account.Credentials(None, None, None)
         client = client_class(credentials=creds)
-        use_jwt.assert_called_with(True)
+        use_jwt.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "transport_class,transport_name",
+    [(transports.RegionInstancesRestTransport, "rest"),],
+)
+def test_region_instances_client_service_account_always_use_jwt_true(
+    transport_class, transport_name
+):
+    with mock.patch.object(
+        service_account.Credentials, "with_always_use_jwt_access", create=True
+    ) as use_jwt:
+        creds = service_account.Credentials(None, None, None)
+        transport = transport_class(credentials=creds, always_use_jwt_access=True)
+        use_jwt.assert_called_once_with(True)
 
 
 @pytest.mark.parametrize("client_class", [RegionInstancesClient,])
