8612bb572eabdd2085224bbdda178af6202175cb
Anthonios Partheniou <partheniou@google.com>
2021-11-10 12:10:48 +0000

    fix(deps): require google-api-core >=2.1.0

--- setup.py
+++ setup.py
@@ -44,10 +44,7 @@ setuptools.setup(
     platforms="Posix; MacOS X; Windows",
     include_package_data=True,
     install_requires=(
-        # NOTE: Maintainers, please do not require google-api-core>=2.x.x
-        # Until this issue is closed
-        # https://github.com/googleapis/google-cloud-python/issues/10566
-        "google-api-core[grpc] >= 1.28.0, <3.0.0dev",
+        "google-api-core[grpc] >= 2.1.0, <3.0.0dev",
         "proto-plus >= 1.13.0",
     ),
     python_requires=">=3.6",
