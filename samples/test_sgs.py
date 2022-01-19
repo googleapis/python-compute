8a45bfced71f8dce6b2916915ab6f64d3086654e
Maciej Strzelczyk <strzelczyk@google.com>
2022-01-19 16:38:37 +0100

    chore(samples): Making linter ignore the test fixtures.

--- samples/test_sgs.py
+++ samples/test_sgs.py
@@ -11,10 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import glob
-import tempfile
 from argparse import Namespace
+import glob
 from pathlib import Path
+import tempfile
 
 import sgs
 
