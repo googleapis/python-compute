835c0dad1e0e81da2df9987540307efed62e34f1
Maciej Strzelczyk <strzelczyk@google.com>
2022-03-01 16:34:38 +0100

    Making lint happy.

--- samples/snippets/tests/test_sample_templates.py
+++ samples/snippets/tests/test_sample_templates.py
@@ -17,15 +17,16 @@ import uuid
 import google.auth
 import pytest
 
-from ..instance_templates.create import create_template
-from ..instance_templates.create_from_instance import create_template_from_instance
-from ..instance_templates.create_with_subnet import create_template_with_subnet
-from ..instance_templates.delete import delete_instance_template
-from ..instance_templates.list import list_instance_templates
-
 # Turning off F401 check because flake8 doesn't recognize using
 # PyTest fixture as parameter as usage.
 from .test_sample_start_stop import compute_instance  # noqa: F401
+
+from ..instance_templates.create import create_template
+from ..instance_templates.create_from_instance import \
+    create_template_from_instance
+from ..instance_templates.create_with_subnet import create_template_with_subnet
+from ..instance_templates.delete import delete_instance_template
+from ..instance_templates.list import list_instance_templates
 
 PROJECT = google.auth.default()[1]
 
