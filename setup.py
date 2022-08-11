0b003f707841334dc0d3b828cc095d13c274a852
Anthonios Partheniou <partheniou@google.com>
2022-08-11 15:44:47 +0000

    fix(deps): require proto-plus >= 1.22.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     include_package_data=True,
     install_requires=(
         "google-api-core[grpc] >= 2.8.0, <3.0.0dev",
-        "proto-plus >= 1.19.7, <2.0.0dev",
+        "proto-plus >= 1.22.0, <2.0.0dev",
         "protobuf >= 3.19.0, <5.0.0dev",
     ),
     python_requires=">=3.7",
