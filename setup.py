186ba045eb6b8cba823c30a442e0b30d0b009bdd
Anthonios Partheniou <partheniou@google.com>
2022-10-06 18:46:12 +0000

    fix(deps): allow protobuf 3.19.5

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     install_requires=(
         "google-api-core[grpc] >= 2.10.1, <3.0.0dev",
         "proto-plus >= 1.22.0, <2.0.0dev",
-        "protobuf >= 3.20.2, <5.0.0dev",
+        "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
     ),
     python_requires=">=3.7",
     classifiers=[
