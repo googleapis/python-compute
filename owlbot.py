3788a98209e8065f88260d0e06e01ff63bb14c2e
Anthonios Partheniou <partheniou@google.com>
2023-09-14 11:39:08 -0400

    build: move to standard owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -35,25 +35,7 @@ for library in s.get_staging_dirs(default_version):
     if clean_up_generated_samples:
         shutil.rmtree("samples/generated_samples", ignore_errors=True)
         clean_up_generated_samples = False
-
-    # Work around gapic generator bug https://github.com/googleapis/gapic-generator-python/issues/1083
-    s.replace(
-        library / "google/cloud/**/types/compute.py",
-        """A request message for InstanceGroupManagers.AbandonInstances.
-    See the method description for details.\n
-    Attributes""",
-        """A request message for InstanceGroupManagers.AbandonInstances.
-    See the method description for details.\n
-    .. _oneof: https://proto-plus-python.readthedocs.io/en/stable/fields.html#oneofs-mutually-exclusive-fields\n
-    Attributes"""
-    )
-
-    # Work around formatting issues with docstrings
-    s.replace(library / "google/cloud/**/types/compute.py", """\"IT_\"""", """`IT_`""")
-    s.replace(library / "google/cloud/**/types/compute.py", """\"NS_\"""", """`NS_`""")
-    s.replace(library / "google/cloud/**/types/compute.py", """\/\[a-zA-Z0-9-_\]\/""", """`[a-zA-Z0-9-_]`""")
-
-    s.move([library], excludes=["**/gapic_version.py", "README.rst"])
+    s.move([library], excludes=["**/gapic_version.py"])
 s.remove_staging_dirs()
 
 # ----------------------------------------------------------------------------
@@ -61,11 +43,11 @@ s.remove_staging_dirs()
 # ----------------------------------------------------------------------------
 
 templated_files = gcp.CommonTemplates().py_library(
-    cov_level=98,
+    cov_level=100,
     microgenerator=True,
     versions=gcp.common.detect_versions(path="./google", default_first=True),
 )
-s.move(templated_files, excludes=[".coveragerc", ".github/release-please.yml", "README.rst"])
+s.move(templated_files, excludes=[".coveragerc", ".github/release-please.yml"])
 
 python.py_samples(skip_readmes=True)
 
